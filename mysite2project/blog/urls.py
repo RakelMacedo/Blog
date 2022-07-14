bab5f8251d5d0d9b89993b313f922313b23c6615
Rakel <macedorakel35@gmail.com>
2022-07-14 15:05:23 -0300

    Update: setup urls to use templates

--- mysite2project/blog/urls.py
+++ mysite2project/blog/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from blog.views import post_view
 
 urlpatterns = [
-    path('', post_view.PostView.as_view(), name='home')
+    path('', post_view.PostView.as_view(), name='home'),
+    path('<slug:slug>/', post_view.PostDetail.as_view(), name='post_detail'),
 ]
